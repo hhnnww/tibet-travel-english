48baa9599ac4dcfe4ba2baa300fffb23c9658e74
wuweihua <175990271@qq.com>
2026-05-06 21:22:54 +0800

    000

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-
-import * as schema from "./schema.ts";
-
-const dbUrl = process.env.DATABASE_URL;
-if (!dbUrl) {
-	throw new Error("DATABASE_URL environment variable is required");
-}
-export const db = drizzle(dbUrl, { schema });
